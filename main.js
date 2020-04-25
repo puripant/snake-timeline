97be42366ad301117e6cf807acc02e3f6dee0a8f
Puripant <puripant@gmail.com>
2020-04-26 00:45:44 +0700

    Add two possible transitions

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const width = (window.innerWidth > 500) ? 480 : 300;
-const height = 5000;
+const height = 500;
 const margin = { left: 25, right: 25, top: 50, bottom: 10 };
 // const width_factor = (window.innerWidth > 500) ? 1 : 1.5;
+
+const data = [100, -10, 100, -50, 200, -100, 80, -100, 150, -20, 70]
+const snake_thickness = 10;
+const snake_gap = 2;
 
 // const date_scale = d3.scaleTime()
 //   .domain([new Date(2020, 0, 13), Date.now()])
@@ -26,6 +30,60 @@ const svg = d3.select('#chart')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 const t = svg.transition().duration(750);
 
+let snake = (forward, w, h, r=h/2) =>
+  forward ?
+    `M0,${h} v${-(h-r)} a${r},${r} 0 0 1 ${r},${-r} h${w-r} v${h-r} a${r},${r} 0 0 1 ${-r},${r} z` :
+    `M${w-r},0 a${r},${r} 0 0 1 ${r},${r} v${h-r} h${-(w-r)} a${r},${r} 0 0 1 ${-r},${-r} v${-(h-r)} z`
+
+let draw = () => {
+  svg.selectAll("*").remove();
+
+  const duration_move_down = 100;
+
+  let sum = 0;
+  let sum_abs = 0;
+  data.forEach((d, i) => {
+    if (layouts.snake) {
+      svg.append('path')
+        .style('fill', (d > 0) ? "salmon" : "crimson")
+        .attr('d', snake(d > 0, Math.abs(d), snake_thickness))
+        .style('opacity', 0)
+        .attr("transform", `translate(${sum + ((d > 0) ? 0 : d)},0)`)
+        .transition()
+          .delay(i*500)
+          .style('opacity', 1)
+          .attr("transform", `translate(${sum + ((d > 0) ? 0 : d)},${(data.length - i)*(snake_thickness + snake_gap)})`)
+    } else if (layouts.fish) {
+      let rect = svg.append('rect')
+        .style('fill', (d > 0) ? "salmon" : "crimson")
+        .attr('height', snake_thickness)
+        .attr('width', 0)
+        .attr('x', (d > 0) ? 0 : -d)
+        .attr('y', 0)
+        .attr('transform', `translate(${sum + ((d > 0) ? 0 : d)},0)`)
+        // .attr("transform", `translate(${sum + ((d > 0) ? 0 : d)},${(data.length - i)*(snake_thickness + snake_gap)})`)
+        .transition()
+          .duration(Math.abs(d)*10)
+          .delay(sum_abs*10 + duration_move_down)
+          .attr('width', Math.abs(d))
+          .attr('x', 0);
+
+      for (let j = i+1; j < data.length; j++) {
+        rect = rect.transition()
+          .duration(Math.abs(data[j])*10)
+          .attr('y', (j - i)*(snake_thickness + snake_gap));
+      }
+    }
+    sum += d;
+    sum_abs += Math.abs(d);
+  })
+  // svg.append('path')
+  //   .attr('d', snake(true, 200, snake_thickness))
+  //   .attr("transform", "translate(0,10)");
+  // svg.append('path')
+  //   .attr('d', snake(false, 100, snake_thickness))
+  //   .attr("transform", "translate(0,50)");
+}
 // let names = [];
 // let cells = [];
 // let draw = () => {
@@ -127,7 +185,7 @@ const buttons = {
   snake: d3.select('#transform-button-snake'),
   fish: d3.select('#transform-button-fish'),
 }
-let layouts = { snake: true, fish: false };
+let layouts = { snake: false, fish: true };
 let transform = shape => {
   if (!layouts[shape]) {
     for (let key in layouts) {
@@ -155,6 +213,7 @@ let date_from_text = (text) => {
 }
 let text_from_date = date => `${date.getDate()} ${months[date.getMonth()]}`
 
+draw();
 // d3.csv(data_filename).then(data => {
 //   names = data;
 //   names_by_date = {};
